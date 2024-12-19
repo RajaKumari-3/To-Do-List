38541e157966c0aa2bc44075fd0cc03fe8df4eab
RajaKumari-3 <rajakumariradhakrishnan50@gmail.com>
2024-12-19 20:39:55 +0530

    Added files from local machine

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTask() {
+      const taskInput = document.getElementById('taskInput');
+      const taskList = document.getElementById('taskList');
+
+      if (taskInput.value.trim() === '') {
+        alert('Please enter a task.');
+        return;
+      }
+
+      const li = document.createElement('li');
+      li.textContent = taskInput.value;
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.className = 'delete-btn';
+      deleteBtn.textContent = 'Delete';
+
+      deleteBtn.onclick = function() {
+        taskList.removeChild(li);
+      };
+
+      li.appendChild(deleteBtn);
+      taskList.appendChild(li);
+
+      taskInput.value = '';
+    }
